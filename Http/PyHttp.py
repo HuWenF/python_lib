540a4b4fbfe6be38979bf6618a0bf8954089e09f
胡文峰 <qw4749105@yahoo.com>
2018-09-16 20:48:21 +0800

    http请求

---
+++ Http/PyHttp.py
@@ -0,0 +1,51 @@
+import httplib, urllib
+import urlparse
+
+class HttpReq(object):
+    @staticmethod
+    def __get_http_host(host):
+        if not host.startswith("http://"):
+            raise RuntimeError("please start use 'http://'")
+        parse_res = urlparse.urlparse(host)
+        return {"host":parse_res.netloc, "path": parse_res.path, "query": parse_res.query}
+
+    @staticmethod
+    def __combine_query(param):
+        if not isinstance(param, dict):
+            raise RuntimeError("param must a dict type")
+        res_tmp = []
+        for k,v in param.items():
+            res_tmp.append("%s=%s" %(k,v))
+        return '&'.join(res_tmp)
+
+    def get_request(self,host, param, headers=None):
+        parse_res = self.__get_http_host(host)
+        conn = httplib.HTTPConnection(parse_res["host"])
+        if parse_res["query"]:
+            query_part = self.__combine_query(param) + "&" + parse_res["query"]
+        else:
+            query_part = self.__combine_query(param)
+        req_path = "/" + parse_res["path"] + "?" + query_part
+        conn.request("GET",req_path, headers= headers if headers else {})
+        return conn.getresponse()
+
+    @staticmethod
+    def post_request(host, param, headers=None):
+        params = urllib.urlencode(param)
+        conn = httplib.HTTPConnection(host)
+        conn.request("POST", params, headers)
+        return conn.getresponse()
+
+
+
+
+
+
+host = "http://www.baidu.com"
+param = {"123":"321"}
+
+a = HttpReq()
+b = a.get_request("http://www.baidu.com", param)
+print b
+
+
